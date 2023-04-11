161a7558b092403ebc3fafcde5927f89f08d24d7
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-04-11 19:29:46 +0530

    Added: Added basic layout and styling for app.

---
+++ src/sections/footerSection.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FooterSection = () => {
+  return <div>Footer Section</div>;
+};
+
+export default FooterSection;
---
+++ src/sections/homeSection.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomeSection = () => {
+  return <div>Home Section</div>;
+};
+
+export default HomeSection;
