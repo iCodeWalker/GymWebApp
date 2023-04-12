7f8274367f28454a2d119c92688bf76c95af5328
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-04-12 15:54:19 +0530

    Added: header section layout

--- src/sections/headerSection.js
+++ src/sections/headerSection.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import "./headerSection.scss";
+
 const HeaderSection = () => {
-  return <div>Header Section</div>;
+  return (
+    <div className="header-container">
+      <h1>Gym Name</h1>
+      <p>
+        This library encourages your applications to be more accessible and
+        allows you to get your tests closer to using your components the way a
+        user will, which allows your tests to give you more confidence that your
+        application will work when a real user uses it.
+      </p>
+    </div>
+  );
 };
 
 export default HeaderSection;
