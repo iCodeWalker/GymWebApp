a9325c29b03dd08ed385e860adbad6554b16aaf6
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-04-21 22:44:01 +0530

    Added: useContext for consuming

--- src/sections/homeSection.js
+++ src/sections/homeSection.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Data } from "../App";
 
 import "./homeSection.scss";
 
 const HomeSection = () => {
-  return <div className="home-container">Home Section</div>;
+  const data = useContext(Data);
+
+  return (
+    <div className="home-container">
+      <h1>{data.HomeSection.heading}</h1>
+    </div>
+  );
 };
 
 export default HomeSection;
